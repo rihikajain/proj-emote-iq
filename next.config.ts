abaf00d969a159e274d7e2e9f55488acf3a3a07c
Rihika <116342520+rihikajain@users.noreply.github.com>
2025-11-07 23:43:20 +0530

    minor changes in config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    // Ignore ESLint errors during production build
+    esmExternals: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
